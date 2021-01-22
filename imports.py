c6a51ad1a6a7f55081f8d2fb93218427d2d6aae0
Owen <owen.allemang@gmail.com>
2021-01-22 14:29:07 +0000

    small graphs

--- imports.py
+++ imports.py
@@ -1,3 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def f(x):
+    return x ** 2
+
+
+x = np.linspace(-10, 10)
+y = f(x)
+
+plt.plot(x, y, c="r")
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.title("Plot")
+plt.show()
