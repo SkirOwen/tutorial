8c3efe5a3a09c9146d38d1453ac3ad2f3c48b7a6
Owen <owen.allemang@gmail.com>
2021-01-22 03:47:15 +0000

    if else

---
+++ condtions.py
@@ -0,0 +1,25 @@
+a = 2
+
+if a == 1:
+    print("a = 1")
+else:
+    print("nope")
+
+# do an if else statement to check if a is positive, and print so if it is and print negatif if not
+
+
+# same but check if a is different then 10 without an else
+
+
+if a >= 2:
+    pass    # do nothing
+elif a < 1:
+    pass
+else:
+    pass
+
+# do an if elif else statement to check if a is even, if not check if it is multiple of 3 else print something
+
+
+
+
